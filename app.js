87f63caca3c451c9c7c724d8b72d32b036e01458
Christopher Van <cvan@mozilla.com>
2015-02-11 16:16:38 -0800

    add support for tracking movements via mobile gamepad

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ var ws = new WebSocketServer({
 var Lobby = require('./modules/lobby');
 var lobby = new Lobby();
 
+// gamepad players
+var players = {};
 
 // socket connection
 ws.on('connection', function(client) {
@@ -36,5 +38,33 @@ ws.on('connection', function(client) {
         id: client.id
     });
 
-    lobby.join(client);
+    client.on('register.game', function(playerID) {
+        console.log('register.game', playerID);
+        players[playerID] = client;
+
+        // We don't want to spam the gamepad with events from the lobby.
+        lobby.join(client);
+    });
+
+    client.on('register.gamepad', function(playerID) {
+        console.log('register.gamepad', playerID);
+        if (!(playerID in players)) {
+            return console.warn('[register.gamepad] Player %s not yet in players:',
+                playerID, players);
+        }
+    });
+
+    client.on('gamepad', function(data) {
+        console.log('[gamepad] Sending gamepad message to client:', data);
+
+        var playerID = data.player;
+        var playerClient = players[playerID];
+
+        if (!(playerID in players)) {
+            return console.error('[gamepad] Player %s not yet in players:',
+                playerID, players);
+        }
+
+        playerClient.send('gamepad', data);
+    });
 });
